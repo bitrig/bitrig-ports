c57e1a99f291515e6871847cd491bf783f9332a5
Bjorn Ketelaars <bjorn.ketelaars@hydroxide.nl>
2012-06-15 21:47:56 +0200

    There is no need to use g77-old on i386 and AMD64. As such, default to g77. While being here remove the gcc4 dependency for gfortran
    thib: "If it works, I see no reason not to move forward..."

--- infrastructure/mk/fortran.port.mk
+++ infrastructure/mk/fortran.port.mk
@@ -7,35 +7,18 @@ ERRORS += "Fatal: need to specify MODFORTRAN_COMPILER"
 .endif
 
 .if ${MODFORTRAN_COMPILER:L} == "g77"
-.  if ${COMPILER_VERSION:L:Mgcc[34]*}
-_MODFORTRAN_LIB_DEPENDS_G77 = devel/libf2c
-_MODFORTRAN_WANTLIB_G77 = g2c
-_MODFORTRAN_BUILD_DEPENDS_G77 = lang/g77 devel/libf2c
-.  else
-_MODFORTRAN_LIB_DEPENDS_G77 = devel/libf2c-old
-_MODFORTRAN_WANTLIB_G77 += g2c
-_MODFORTRAN_BUILD_DEPENDS_G77 = lang/g77-old devel/libf2c-old
-.  endif
-MODFORTRAN_LIB_DEPENDS += ${_MODFORTRAN_LIB_DEPENDS_G77}
-MODFORTRAN_WANTLIB += ${_MODFORTRAN_WANTLIB_G77}
-MODFORTRAN_BUILD_DEPENDS += ${_MODFORTRAN_BUILD_DEPENDS_G77}
+MODFORTRAN_LIB_DEPENDS += devel/libf2c
+MODFORTRAN_WANTLIB += g2c
+MODFORTRAN_BUILD_DEPENDS += lang/g77 devel/libf2c
 MODFORTRAN_post-patch = \
 if test -e /usr/bin/g77 -o -e /usr/bin/f77; then \
     echo "Error: remove old fortran compiler /usr/bin/f77 /usr/bin/g77"; \
     exit 1; \
 fi
 .elif ${MODFORTRAN_COMPILER:L} == "gfortran"
-.  if ${COMPILER_VERSION:L:Mgcc4}
-_MODFORTRAN_LIB_DEPENDS_GFORTRAN = lang/gfortran,-lib
-_MODFORTRAN_WANTLIB_GFORTRAN = gfortran
-_MODFORTRAN_BUILD_DEPENDS_GFORTRAN = lang/gfortran
-.  else
-MODULES += gcc4
-MODGCC4_LANGS += fortran
-.  endif
-MODFORTRAN_LIB_DEPENDS += ${_MODFORTRAN_LIB_DEPENDS_GFORTRAN}
-MODFORTRAN_WANTLIB += ${_MODFORTRAN_WANTLIB_GFORTRAN}
-MODFORTRAN_BUILD_DEPENDS += ${_MODFORTRAN_BUILD_DEPENDS_GFORTRAN}
+MODFORTRAN_LIB_DEPENDS += lang/gfortran,-lib
+MODFORTRAN_WANTLIB += gfortran
+MODFORTRAN_BUILD_DEPENDS += lang/gfortran
 .else
 ERRORS += "Fatal: MODFORTRAN_COMPILER must be one of: g77 gfortran"
 .endif
