a2e27a31baa8572ac3d4f5aec034c9f38d672bab
Dale Rahn <drahn@dalerahn.com>
2014-03-19 11:17:54 -0400

    Revert "Default to gcc 4.8 for ports using gcc4 module."
    This reverts commit f4730e836122f15cb82933877e8decb7493f7391.
    
    Not ready yet.

--- infrastructure/mk/gcc4.port.mk
+++ infrastructure/mk/gcc4.port.mk
@@ -1,6 +1,2 @@
-.if ${MACHINE_ARCH} == "hppa" || ${MACHINE_ARCH} == "alpha"                                                                                                   
-MODGCC4_VERSION?=4.6                                                                                                                                          
-.else                                                                                                                                                         
-MODGCC4_VERSION?=4.8
-.endif
+MODGCC4_VERSION?=4.6
 .include "${PORTSDIR}/lang/gcc/${MODGCC4_VERSION}/gcc4.port.mk"
