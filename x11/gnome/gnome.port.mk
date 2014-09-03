3cdd601f742b734f50dd634d1267e44b807fad08
Imported From OpenBSD <noreply@bitrig.org>
2014-09-03 19:42:16 +0000

    use += to set PORTSCOUT so consumers of this module can append their own stuffs to it
    as discussed with aja@
    
    Written by: Jasper Lievisse Adriaanse <jasper@openbsd.org>

--- x11/gnome/gnome.port.mk
+++ x11/gnome/gnome.port.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: gnome.port.mk,v 1.91 2014/09/03 09:53:55 ajacoutot Exp $
+# $OpenBSD: gnome.port.mk,v 1.92 2014/09/03 19:42:16 jasper Exp $
 #
 # Module for GNOME related ports
 
@@ -7,7 +7,7 @@ MODCLANG_ARCHS =        amd64 i386
 MODCLANG_LANGS =        c c++
 
 .if (defined(GNOME_PROJECT) && defined(GNOME_VERSION))
-PORTSCOUT=		limitw:1,even
+PORTSCOUT+=		limitw:1,even
 DISTNAME=		${GNOME_PROJECT}-${GNOME_VERSION}
 VERSION=		${GNOME_VERSION}
 HOMEPAGE?=		https://wiki.gnome.org/
