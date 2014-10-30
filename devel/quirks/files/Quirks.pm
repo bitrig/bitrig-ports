8f583447b9a0579b6e19102597999e936ec370c0
Imported From OpenBSD <noreply@bitrig.org>
2014-10-30 17:04:22 +0000

    update to symon 2.87
    - N.B. Packages are re-arranged. Previously we had symon-mon (client only),
    symon-mux (mux only), and symon (both client + mux - i.e. the combination of
    the two other packages). Now we just have two non-overlapping packages
    symon+symux, as is normal for multi-packages in the tree.
    
    pkg_add -u will handle renaming but not splitting, so if you used the
    "combined" package you will need to "pkg_add symux" after this update.
    
    Written by: Stuart Henderson <sthen@openbsd.org>

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.191 2014/10/30 11:09:11 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.192 2014/10/30 17:04:22 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -356,6 +356,8 @@ my $stem_extensions = {
 
 # Bitrig 0.1
 	'py3-distribute' => 'py3-setuptools',
+	'symon-mon' => 'symon',
+	'symon-mux' => 'symux',
 };
 
 # reasons for obsolete packages
