5f4a512ee15baa7584281dbf9f4191b86d394696
Dale Rahn <drahn@dalerahn.com>
2016-01-13 19:26:03 -0500

    work around locating thread data in arch64 static binaries.
    For reasons not quite fathomed, the ELF header is not located
    in the first LOAD section of the elf image, thus the hack
    to located the ELF header and find the thread address and offset
    does not work. Introduce symbols which can be used for this.

--- bitrig/bitrig-binutils-2.24/files/aarch64bitrig.sh
+++ bitrig/bitrig-binutils-2.24/files/aarch64bitrig.sh
@@ -1,2 +1,4 @@
 . ${srcdir}/emulparams/aarch64elf.sh
 . ${srcdir}/emulparams/elf_obsd.sh
+TDATA_START_SYMBOLS='PROVIDE_HIDDEN (__tdata_start = .);';
+TDATA_END_SYMBOLS='PROVIDE_HIDDEN (__tdata_end = .);';
