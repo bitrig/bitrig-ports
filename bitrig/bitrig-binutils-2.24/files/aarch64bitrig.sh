964e83bcfed0287216b6152ecbc80f66fc1ef0ea
Dale Rahn <drahn@dalerahn.com>
2016-01-26 19:04:24 -0500

    Bitrig is not embedded, For share libraries, Phdr must be in load area.

--- bitrig/bitrig-binutils-2.24/files/aarch64bitrig.sh
+++ bitrig/bitrig-binutils-2.24/files/aarch64bitrig.sh
@@ -1,4 +1,5 @@
 . ${srcdir}/emulparams/aarch64elf.sh
 . ${srcdir}/emulparams/elf_obsd.sh
+EMBEDDED= # This gets us program headers mapped as part of the text segment.
 TDATA_START_SYMBOLS='PROVIDE_HIDDEN (__tdata_start = .);';
 TDATA_END_SYMBOLS='PROVIDE_HIDDEN (__tdata_end = .);';
