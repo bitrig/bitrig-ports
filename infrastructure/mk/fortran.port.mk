dc2a22787b06522656ef8231bb93d913be1b25f4
Marco Peereboom <marco@peereboom.us>
2016-04-08 13:47:41 -0500

    Remove g77.
    g77 is openbsd specific so remove it and use gfortran instead.

--- infrastructure/mk/fortran.port.mk
+++ infrastructure/mk/fortran.port.mk
@@ -1,12 +1,13 @@
 # $OpenBSD: fortran.port.mk,v 1.12 2010/11/20 19:57:30 espie Exp $
 
-MODFORTRAN_COMPILER ?= g77
+MODFORTRAN_COMPILER ?= gfortran
 
 .if empty(MODFORTRAN_COMPILER)
 ERRORS += "Fatal: need to specify MODFORTRAN_COMPILER"
 .endif
 
 .if ${MODFORTRAN_COMPILER:L} == "g77"
+ERRORS += "g77 fortran compiler isn't supported by Bitrig"
 MODFORTRAN_LIB_DEPENDS += devel/libf2c
 MODFORTRAN_WANTLIB += g2c
 MODFORTRAN_BUILD_DEPENDS += lang/g77 devel/libf2c
@@ -19,6 +20,7 @@ fi
 MODFORTRAN_LIB_DEPENDS += lang/gfortran,-lib
 MODFORTRAN_WANTLIB += gfortran
 MODFORTRAN_BUILD_DEPENDS += lang/gfortran
+FC=gfortran
 .else
 ERRORS += "Fatal: MODFORTRAN_COMPILER must be one of: g77 gfortran"
 .endif
